602a9deb8c4f2aa203d03e5cc0e5837d847ef9ed
Sakhii <sakhiii044@gmail.com>
2025-04-01 04:20:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function openLetter() {
+    let envelope = document.getElementById("envelope");
+    let letter = document.getElementById("letter");
+    let button = document.getElementById("openButton");
+
+    // Buka amplop dan munculkan surat
+    envelope.classList.add("opened");
+}
+
